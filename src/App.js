5daf18bb0e83e4e3262fc9b25435e7280d92cb5e
hafiz <hafizn2013@outlook.com>
2023-03-07 23:39:30 -0800

    random quote generator app for freebootcamp project1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: "",
+      author: ""
+    };
+
+    this.fetchQuote = this.fetchQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+  
+  async fetchQuote() {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    if (response.ok) {
+      this.setState({
+        quote: data.content,
+        author: data.author
+      });
+    } else {
+      console.log("error with fetching quote")
+    }
+  }
+  
+  render() {
+    return (
+      <div id="quote-box">
+        <div>
+          <div id="text">
+            {this.state.quote}
+          </div>
+          <div id="author">
+            - {this.state.author}
+          </div>
+        </div>
+        <div id="btn-wrapper">
+          <a id="tweet-quote" class="btn btn-primary" href="twitter.com/intent/tweet" target="_top">tweet quote</a>
+        <button id="new-quote" class="btn btn-primary" onClick={this.fetchQuote}>New quote</button>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
